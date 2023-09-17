a96a880acb2e7a0d567e125b9a8769950fc08bd9
David Miller <dmiller2117@gmail.com>
2023-09-17 18:15:29 +0100

    Adding config

---
+++ src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,8 @@
+package com.learnkafka.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@EnableKafka :: needed on older versions, fyi only.
+public class LibraryEventsConsumerConfig {
+}
