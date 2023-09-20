760938d9a585d469401d7fc2d180b8d7f7994634
David Miller <dmiller2117@gmail.com>
2023-09-20 21:53:29 +0100

    Adding JPA entities

---
+++ src/main/java/com/learnkafka/entity/LibraryEvent.java
@@ -0,0 +1,24 @@
+package com.learnkafka.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class LibraryEvent {
+
+    @Id
+    @GeneratedValue
+    private Integer libraryEventId;
+    @Enumerated(EnumType.STRING)
+    private LibraryEventType libraryEventType;
+    @OneToOne(mappedBy = "libraryEvent")
+    private Book book;
+
+}
